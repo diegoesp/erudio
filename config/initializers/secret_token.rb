1e924298554c69ba75e5dc6de605e9d11851c8c6
Diego Espada <diegoesp@gmail.com>
2012-01-24 20:30:41 -0500

    Initial commit for Erudio. Rock & Roll

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erudio::Application.config.secret_token = '2cc18c0cab570d66b9ab3229e994f7463e8198cc711ffb9f937a2980b36e74a7bbfdc2b8ec3267f7b07a2b5fd9598e7433ee46e6cdf15243787ee5bae035a37d'
