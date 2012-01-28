2e8141ea6ad13b74ee78675556215970a9310e0d
Rafael Chiti <rafaelchiti@gmail.com>
2012-01-28 18:35:57 -0300

    Agregue un template basico a la home page para que muestre las actividades que estan bindeadas cuando se carga la pagina. Las actividades estan bindeadas a un objeto javascript de acceso global.

--- public/site/scripts/homePage.js
+++ public/site/scripts/homePage.js
@@ -1,1 +1,9 @@
 window.application = {};
+
+//
+// Init logic after page 'loading'
+//
+$(document).ready(function() {
+	activities = window.application.activities;
+	$( "#activityTemplate" ).tmpl().appendTo( "#slideBox1" );
+});
