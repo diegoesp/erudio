0fcfd91e6d016475be07fda5615770b030e2648e
Rafael Chiti <rafaelchiti@gmail.com>
2012-02-03 21:38:47 -0300

    Agregue un objeto a la pagina "searchTerm" que se ira actualizando en la medida que el usuario vaya por el wizard.

--- public/site/scripts/homePage.js
+++ public/site/scripts/homePage.js
@@ -1,4 +1,5 @@
 window.application = {};
+window.application.searchTerm = {};
 
 //
 // Init logic after page 'loading'
@@ -10,7 +11,20 @@ $(document).ready(function() {
 });
 
 
-function selectActivity() {
+//
+// When the user selects an activity from the first step on the wizard
+// the searchTerm object is updated and the wizard moves to the second step.
+//
+function selectActivity(htmlContainer) {
+	id = $(htmlContainer).find("input[type=hidden]").get(0).value;
+	window.application.searchTerm.activityId = id;
 	right();
 }
 
+
+//
+// TODO(rafael.chiti): Remove this. Just for debugging.
+//
+function alertSearchTerm() {
+	alert(JSON.stringify(window.application.searchTerm));
+}
