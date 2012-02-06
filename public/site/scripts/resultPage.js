b6799d7492545307f505c1ce2010cba8be174b3c
Rafael Chiti <rafaelchiti@gmail.com>
2012-02-06 08:13:54 -0300

    Agregue algunos comentarios

--- public/site/scripts/resultPage.js
+++ public/site/scripts/resultPage.js
@@ -1,13 +1,17 @@
 window.application = {};
 window.application.searchTerm = {};
 
-checkBoxes = undefined;
-
+//
+// DOCUMENT READY
+//
 // Init logic after page 'loading'
+//
 $(document).ready(function() {
 	
+	// Initialize the SweetCheckboxes by calling the config method from the library
 	configureSweetCheckboxes();
 
+	// Bind the "results" JSON object to the template and render it.
 	var template = _.template($("#resultTemplate")[0].text, {data: window.application.results});
     $("#resultsColumn").html(template);
 
@@ -15,14 +19,27 @@ $(document).ready(function() {
 	// synch them with the search term status from the server.
 	// TODO(rafael.chiti) This logic should be on the server, changing the 
 	// status of the chekboxes after loading the page is not "nice".
-	checkBoxes = $(".sweetCheckbox");
-	checkBoxes.each(function(index){
+	$(".sweetCheckbox").each(function(index){
 		if (window.application.searchTerm[this.title] === true) {
 			$(this).trigger('turnOn');		
 		} 
 	});
 
 	// Configure the ToolTipsy tool.
+	initializeTooltipsy();
+
+});
+
+// Update the searchTerm JSON object based on the state of the 'clicked' checkbox.
+// Each time the user clicks any of the options from the filter
+// this functions is triggered.
+function updateBoolFilter(checkBox, filterOption) {
+	window.application.searchTerm[filterOption] = !$(checkBox).triggerHandler('isOn');
+}
+
+
+// Initialize and configure the tooltips for this page (tooltipsy library)
+function initializeTooltipsy() {
 	$('.hasTooltipsy').tooltipsy({
 	    offset: [-10, 0],
 	    show: function (e, $el) {
@@ -50,28 +67,17 @@ $(document).ready(function() {
 	        'text-shadow': 'none'
 	    }
 	});
-
-});
-
-// Each time the user clicks any of the options from the filter
-// this functions is triggered.
-function updateBoolFilter(checkBox, filterOption) {
-	window.application.searchTerm[filterOption] = !$(checkBox).triggerHandler('isOn');
 }
 
 
-
-
-
-////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 //
-// 		REMOVE THIS FUNCTIONS WHEN FINISHED
+// 	REMOVE THIS FUNCTIONS WHEN FINISHED (DEBBUGING BOX)
 //
-
+//////////////////////////////////////////////////////////////
 
 // TODO(rafael.chiti): Remove this. Just for debugging.
 function alertSearchTerm() {
 
 	alert(JSON.stringify(window.application.searchTerm));
-	
 }
