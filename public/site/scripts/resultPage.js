b5144845d0a390aa4b706849528b1721c5cb5d46
Rafael Chiti <rafaelchiti@gmail.com>
2012-02-05 23:37:44 -0300

    Agregue una libreria para hacer pop-ups, hay que probarla pero parece ser util. http://tooltipsy.com/

--- public/site/scripts/resultPage.js
+++ public/site/scripts/resultPage.js
@@ -22,6 +22,34 @@ $(document).ready(function() {
 		} 
 	});
 
+	$('.hasTooltipsy').tooltipsy({
+	    offset: [-10, 0],
+	    show: function (e, $el) {
+	        $el.css({
+	            'left': parseInt($el[0].style.left.replace(/[a-z]/g, '')) - 50 + 'px',
+	            'opacity': '0.0',
+	            'display': 'block'
+	        }).animate({
+	            'left': parseInt($el[0].style.left.replace(/[a-z]/g, '')) + 50 + 'px',
+	            'opacity': '1.0'
+	        }, 300);
+	    },
+	    hide: function (e, $el) {
+	        $el.slideUp(100);
+	    },
+	    css: {
+	        'padding': '10px',
+	        'max-width': '200px',
+	        'color': '#303030',
+	        'background-color': '#f5f5b5',
+	        'border': '1px solid #deca7e',
+	        '-moz-box-shadow': '0 0 10px rgba(0, 0, 0, .5)',
+	        '-webkit-box-shadow': '0 0 10px rgba(0, 0, 0, .5)',
+	        'box-shadow': '0 0 10px rgba(0, 0, 0, .5)',
+	        'text-shadow': 'none'
+	    }
+	});
+
 });
 
 // Each time the user clicks any of the options from the filter
