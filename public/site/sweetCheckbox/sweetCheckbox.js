30e713b2f356d50c4a710b641f89735788694602
Rafael Chiti <rafaelchiti@gmail.com>
2012-02-07 08:28:32 -0300

    Replacing the js for the sweetCheckbox component for the last version

--- public/site/sweetCheckbox/sweetCheckbox.js
+++ public/site/sweetCheckbox/sweetCheckbox.js
@@ -2,47 +2,58 @@ function configureSweetCheckboxes() {
 
 	$(".sweetCheckbox").each(function(){
 		
+		sweetCheckbox = $(this);
+		
 		// TODO(rafael.chiti): Is this necessary?, this should be manage differently.
 		// By default set the "off" background to the checkbox
-		$(this).addClass("sweetCheckboxBGOff");
+		sweetCheckbox.addClass("sweetCheckboxBGOff");
 		
 		// Create the piece of html that will be attached to each 'div checkbox'
 		innerHtml = "<div class=\"sweetCheckboxBallContainer\"> <span class=\"sweetCheckboxBall sweetCheckboxBallOff\"></span> <input type=\"hidden\" value=\"false\"/> </div>";
 		
-		$(this).append(innerHtml);
+		sweetCheckbox.append(innerHtml);
+		
+		// Iterate the DOM only once and get the "input" and "span" element.
+		input = $($(this).find("input").get(0));
+		span = $($(this).find("span").get(0));
+		
 		
 		// Event that will be triggered each time the user 
 		// clicks on the checkbox
-		var clicked = function() {
-			if ($($(this).find("input").get(0)).attr("value") === 'true') {
-				$(this).triggerHandler("turnOff");
+		var clicked = function(event) {
+			if (event.data.input.attr("value") === 'true') {
+				event.data.sweetCheckbox.triggerHandler("turnOff");
 			} else {
-				$(this).triggerHandler("turnOn");			
+				event.data.sweetCheckbox.triggerHandler("turnOn");			
 			}
 		}
 		
 		// Change the checkbox to "on" satate
-		var turnOn = function() {
-			$($(this).find("span").get(0)).switchClass( "sweetCheckboxBallOff", "sweetCheckboxBallOn", 500 );
-			$(this).switchClass( "sweetCheckboxBGOff", "sweetCheckboxBGOn", 200 );
-			$($(this).find("input").get(0)).attr('value', 'true');
+		var turnOn = function(event) {
+			event.data.span.switchClass( "sweetCheckboxBallOff", "sweetCheckboxBallOn", 500 );
+			event.data.sweetCheckbox.switchClass( "sweetCheckboxBGOff", "sweetCheckboxBGOn", 200 );
+			event.data.input.attr('value', 'true');
+
 		}
 
 		// Change the checkbox to "off" satate		
-		var turnOff = function() {
-			$($(this).find("span").get(0)).switchClass( "sweetCheckboxBallOn", "sweetCheckboxBallOff", 500 );
-			$(this).switchClass( "sweetCheckboxBGOn", "sweetCheckboxBGOff", 200 );		
-			$($(this).find("input").get(0)).attr('value', 'false');
+		var turnOff = function(event) {
+			event.data.span.switchClass( "sweetCheckboxBallOn", "sweetCheckboxBallOff", 500 );
+			event.data.sweetCheckbox.switchClass( "sweetCheckboxBGOn", "sweetCheckboxBGOff", 200 );		
+			event.data.input.attr('value', 'false');
 		}
 		
-		var isOn = function() {
-			return $($(this).find("input").get(0)).attr('value') === 'true';
+		var isOn = function(event) {
+			return event.data.input.attr('value') === 'true';
 		}
 		
-		$(this).bind('turnOn', turnOn);
-		$(this).bind('turnOff', turnOff);
-		$(this).bind('isOn', isOn);
-		$(this).bind('click', clicked);
+		// Here the event data object is used when calling 'jQuery.bind' (more information http://api.jquery.com/bind/)
+		// The functions binded are using the 'event' object provided by jQuery in order to get the 
+		// object literal that is being passed here (second argument of bind function). 
+		sweetCheckbox.bind('turnOn', {sweetCheckbox: sweetCheckbox, span: span, input: input}, turnOn);
+		sweetCheckbox.bind('turnOff',{sweetCheckbox: sweetCheckbox, span: span, input: input}, turnOff);
+		sweetCheckbox.bind('isOn', {input: input}, isOn);
+		sweetCheckbox.bind('click', {sweetCheckbox: sweetCheckbox, span: span, input: input}, clicked);
 	});	
 	
 }
