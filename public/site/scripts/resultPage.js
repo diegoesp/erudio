f828b30c4b3782fac707a89257cc002ada21515f
Rafael Chiti <rafaelchiti@gmail.com>
2012-02-07 23:12:35 -0300

    Cambios menores, actualizacion del componente SweetCheckbox

--- public/site/scripts/resultPage.js
+++ public/site/scripts/resultPage.js
@@ -42,19 +42,6 @@ function updateBoolFilter(checkBox, filterOption) {
 function initializeTooltipsy() {
 	$('.hasTooltipsy').tooltipsy({
 	    offset: [-10, 0],
-	    show: function (e, $el) {
-	        $el.css({
-	            'left': parseInt($el[0].style.left.replace(/[a-z]/g, '')) - 50 + 'px',
-	            'opacity': '0.0',
-	            'display': 'block'
-	        }).animate({
-	            'left': parseInt($el[0].style.left.replace(/[a-z]/g, '')) + 50 + 'px',
-	            'opacity': '1.0'
-	        }, 300);
-	    },
-	    hide: function (e, $el) {
-	        $el.slideUp(100);
-	    },
 	    css: {
 	        'padding': '10px',
 	        'max-width': '200px',
