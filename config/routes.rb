0efcf42ecfa3be359c4c0b5a524c4e6c590f7c51
Rafael Chiti <rafaelchiti@gmail.com>
2012-07-22 21:02:22 -0300

    Sign up page layout/css No esta terminado, subo hasta aca por si te sirve para continuar laburando.

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 # -*- encoding : utf-8 -*-
 Erudio::Application.routes.draw do
-  
+
   root :to => 'teachers#home'
 
   resources :users do
     get "login", :on => :collection     # /users/login
     get "logout", :on => :collection    # /users/logout
   end
-    
+
   resources :teachers do
     get "home", :on => :collection      # Root
     get "search" , :on => :collection   # /teachers/search
     get "rating", :on => :member        # /1/rating
     get "rate", :on => :member          # /1/rate
   end
-  
+
   resources :zones do
     get "contiguous" , :on => :member   # /1/contiguous
   end
-  
+
   # API caller for testing different APIs
   match "/api_caller", :to => "pages#api_caller"
-  
+
 end
